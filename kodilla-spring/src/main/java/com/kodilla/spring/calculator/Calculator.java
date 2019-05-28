315963cd83e114e2dba30d8a4bb34e5a50dbec8c
Piotr Filarecki <pfilar2@wp.pl>
2019-05-28 15:06:22 +0200

    Small fix

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,7 +1,8 @@
 package com.kodilla.spring.calculator;
 
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
