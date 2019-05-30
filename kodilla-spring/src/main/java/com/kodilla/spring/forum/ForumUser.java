09a522fd8f8fcda70d503eaf95f65972ca5678da
Piotr Filarecki <pfilar2@wp.pl>
2019-05-30 23:36:00 +0200

    Spring

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,16 +1,9 @@
 package com.kodilla.spring.forum;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Component;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Component
-//@SpringBootApplication
 public class ForumUser {
-    public static void main(String[] args) {
-        SpringApplication.run(ForumUser.class, args);
-    }
-
     private String userName;
 
     public ForumUser() {                // inicjacja pola w konstruktorze "na sztywno"
