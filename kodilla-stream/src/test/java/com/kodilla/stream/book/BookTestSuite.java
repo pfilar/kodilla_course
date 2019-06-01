0467dbfe79c75f55b11c174079c41bb3a00920aa
Piotr Filarecki <pfilar2@wp.pl>
2019-06-01 21:53:07 +0200

    Stream

--- kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class BookTestSuite {
     @Test
@@ -21,6 +22,37 @@ public class BookTestSuite {
                 numberOfBooksPublicatedAfter2007++;
             }
         }
+        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+
+    @Test
+    public void testGetListUsingIntStream() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        int numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
+                .filter(n -> books.get(n).getYearOfPublication() > 2007)
+                .map(n -> 1)
+                .sum();
+        Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+
+    @Test
+    public void testGetListUsingIntStreamTwo() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        long numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
+                .filter(n -> books.get(n).getYearOfPublication() > 2007)
+                .count();
         Assert.assertEquals(3, numberOfBooksPublicatedAfter2007);
     }
 }
